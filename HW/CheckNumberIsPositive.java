039b212d255ff68edfddd7309434313e6ad6864d
venkatloyolite <129617440+venkatloyolite@users.noreply.github.com>
2024-08-30 09:43:35 +0530

    Add files via upload

---
+++ HW/CheckNumberIsPositive.java
@@ -0,0 +1,20 @@
+package HW;
+
+public class CheckNumberIsPositive {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		int version=10;
+		if(version>0)
+		{
+			System.out.println("The given number is positive: " +version);
+			
+		}
+		else
+		{
+			System.out.println("The given number is negative: " +version);
+		}
+
+	}
+
+}
